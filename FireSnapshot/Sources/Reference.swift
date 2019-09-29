a370875ffdf3718926f3851d5897bbe32850ab3c
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-09-30 01:45:35 +0900

    implement `@Reference` property wrapper.

---
+++ FireSnapshot/Sources/Reference.swift
@@ -0,0 +1,36 @@
+//
+// Copyright © Suguru Kishimoto. All rights reserved.
+//
+
+import Foundation
+import FirebaseFirestore
+
+@propertyWrapper
+public struct Reference<D>: Codable  where D: Codable {
+    public private(set) var wrappedValue: DocumentReference
+    public init(wrappedValue: DocumentReference) {
+        self.wrappedValue = wrappedValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        wrappedValue = try container.decode(DocumentReference.self)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
+
+    public var projectedValue: Self {
+        self
+    }
+
+    func get(source: FirestoreSource = .default, completion: @escaping Snapshot<D>.DocumentReadResultBlock) {
+        Snapshot<D>.get(wrappedValue, source: source, completion: completion)
+    }
+
+    func listen(includeMetadataChanges: Bool = false, completion: @escaping Snapshot<D>.DocumentReadResultBlock) {
+        Snapshot<D>.listen(wrappedValue, includeMetadataChanges: includeMetadataChanges, completion: completion)
+    }
+}
