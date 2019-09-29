b3f9fcd16f398b5e379c4ee24753f3ce166c8f17
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-09-30 03:58:56 +0900

    improve emulator.sh and test.sh.

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 cd `dirname $0`
 cd ../firebase
+VERSION=1.8.4
+if [[ ! -e ~/.cache/firebase/emulators/cloud-firestore-emulator-v$VERSION.jar ]]; then
+  firebase setup:emulators:firestore
+else
+  echo 'emulators already installed.'
+fi
+
 firebase emulators:start --only firestore
