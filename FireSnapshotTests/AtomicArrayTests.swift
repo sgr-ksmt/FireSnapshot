3a36dba1e5d807e4edf0a9f265a7b1bd763cf148
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-10-07 16:47:59 +0900

    add AtomicArray tests.

---
+++ FireSnapshotTests/AtomicArrayTests.swift
@@ -0,0 +1,148 @@
+//
+// Copyright © Suguru Kishimoto. All rights reserved.
+//
+
+import XCTest
+import FirebaseFirestore
+
+@testable import FireSnapshot
+
+private extension CollectionPaths {
+    static let mocks = CollectionPath<Mock>("mocks")
+}
+
+private struct Mock: Codable {
+    @AtomicArray var languages: [String] = ["ja"]
+}
+
+class AtomicArrayTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+         FirebaseTestHelper.setupFirebaseApp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        FirebaseTestHelper.deleteFirebaseApp()
+    }
+
+    func testUnionUpdate() {
+        let exp = XCTestExpectation(description: #function)
+        let mock = Snapshot(data: .init(), path: .mocks)
+        mock.create { result in
+            switch result {
+            case .success:
+                Snapshot.get(mock.path) { result in
+                    switch result {
+                    case let .success(m):
+                        XCTAssertEqual(m.data.languages, ["ja"])
+                        m.data.$languages.union("en")
+                        XCTAssertEqual(m.data.languages, ["ja", "en"])
+                        m.update { result in
+                            switch result {
+                            case .success:
+                                Snapshot.get(mock.path) { result in
+                                    XCTAssertEqual((try? result.get())?.data.languages, ["ja", "en"])
+                                    exp.fulfill()
+                                }
+                            case let .failure(error):
+                                XCTFail("\(error)")
+                            }
+                        }
+                    case let .failure(error):
+                        XCTFail("\(error)")
+                    }
+                }
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+        }
+        wait(for: [exp], timeout: 10.0)
+    }
+
+    func testRemoveUpdate() {
+        let exp = XCTestExpectation(description: #function)
+        let mock = Snapshot(data: .init(languages: ["ja", "en", "zh"]), path: .mocks)
+        mock.create { result in
+            switch result {
+            case .success:
+                Snapshot.get(mock.path) { result in
+                    switch result {
+                    case let .success(m):
+                        XCTAssertEqual(m.data.languages, ["ja", "en", "zh"])
+                        m.data.$languages.remove("en")
+                        XCTAssertEqual(m.data.languages, ["ja", "zh"])
+                        m.update { result in
+                            switch result {
+                            case .success:
+                                Snapshot.get(mock.path) { result in
+                                    XCTAssertEqual((try? result.get())?.data.languages, ["ja", "zh"])
+                                    exp.fulfill()
+                                }
+                            case let .failure(error):
+                                XCTFail("\(error)")
+                            }
+                        }
+                    case let .failure(error):
+                        XCTFail("\(error)")
+                    }
+                }
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+        }
+        wait(for: [exp], timeout: 10.0)
+    }
+
+    func testAtomicArrayBehavior() {
+        do {
+            var mock = Mock()
+            XCTAssertEqual(mock.languages, ["ja"])
+
+            mock.$languages.union("en")
+            XCTAssertEqual(mock.languages, ["ja", "en"])
+
+            mock.$languages.reset()
+            XCTAssertEqual(mock.languages, ["ja"])
+
+            mock.$languages.union("en")
+            mock.$languages.union("zh")
+            XCTAssertEqual(mock.languages, ["ja", "zh"])
+
+            mock.$languages.reset()
+            XCTAssertEqual(mock.languages, ["ja"])
+
+            mock.$languages.union(["en", "zh"])
+            XCTAssertEqual(mock.languages, ["ja", "en", "zh"])
+        }
+
+
+        do {
+            var mock = Mock(languages: ["ja", "en"])
+            XCTAssertEqual(mock.languages, ["ja", "en"])
+
+            mock.$languages.remove("en")
+            XCTAssertEqual(mock.languages, ["ja"])
+
+            mock.$languages.reset()
+            XCTAssertEqual(mock.languages, ["ja", "en"])
+
+            mock.$languages.remove("en")
+            mock.$languages.remove("ja")
+            XCTAssertEqual(mock.languages, ["en"])
+
+            mock.$languages.reset()
+            XCTAssertEqual(mock.languages, ["ja", "en"])
+
+            mock.$languages.remove(["en", "ja"])
+            XCTAssertEqual(mock.languages, [])
+
+            mock.$languages.reset()
+            XCTAssertEqual(mock.languages, ["ja", "en"])
+
+            mock.$languages.remove("zh")
+            XCTAssertEqual(mock.languages, ["ja", "en"])
+        }
+    }
+}
