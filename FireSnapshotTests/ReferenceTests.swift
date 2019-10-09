9062449396c96fd16e2b70e7b4e5e33b61f1932f
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-10-09 18:27:17 +0900

    wrote ReferenceTests.

--- FireSnapshotTests/ReferenceTests.swift
+++ FireSnapshotTests/ReferenceTests.swift
@@ -3,27 +3,121 @@
 //
 
 import XCTest
+import FirebaseFirestore
+
+@testable import FireSnapshot
+
+private extension CollectionPaths {
+    static let tasks = CollectionPath<Task>("tasks")
+    static let users = CollectionPath<User>("users")
+}
+
+private struct User: SnapshotData {
+    var name: String = "Mike"
+}
+
+private struct Task: SnapshotData {
+    var title: String
+    @Reference<User> var author = .dummy()
+}
 
 class ReferenceTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        FirebaseTestHelper.setupFirebaseApp()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        FirebaseTestHelper.deleteFirebaseApp()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGetReference() {
+        let exp = expectation(description: #function)
+        let user = Snapshot(data: .init(), path: .users)
+        user.create { result in
+            switch result {
+            case .success:
+                let task = Snapshot(data: .init(title: "task"), path: .tasks)
+                task.author = user.reference
+                task.create { result in
+                    switch result {
+                    case .success:
+                        Snapshot.get(task.path) { result in
+                            switch result {
+                            case let .success(t):
+                                t.$author.get { result in
+                                    XCTAssertEqual((try? result.get())?.name, user.name)
+                                    exp.fulfill()
+                                }
+                            case let .failure(error):
+                                XCTFail("\(error)")
+                            }
+                        }
+                    case let .failure(error):
+                        XCTFail("\(error)")
+                    }
+                }
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
         }
+
+        wait(for: [exp], timeout: 10.0)
     }
 
+    func testGetReferenceFailed() {
+        let exp = expectation(description: #function)
+        let user = Snapshot(data: .init(), path: .users)
+        user.create { result in
+            switch result {
+            case .success:
+                let task = Snapshot(data: .init(title: "task"), path: .tasks)
+                task.author = CollectionPaths.users.document("xxx").documentReference
+                task.create { result in
+                    switch result {
+                    case .success:
+                        Snapshot.get(task.path) { result in
+                            switch result {
+                            case let .success(t):
+                                t.$author.get { result in
+                                    switch result {
+                                    case .success:
+                                        XCTFail()
+                                    case .failure:
+                                        exp.fulfill()
+                                    }
+                                }
+                            case let .failure(error):
+                                XCTFail("\(error)")
+                            }
+                        }
+                    case let .failure(error):
+                        XCTFail("\(error)")
+                    }
+                }
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+        }
+
+        wait(for: [exp], timeout: 10.0)
+    }
+
+    func testEmptyReferenceError() {
+        let exp = expectation(description: #function)
+        let task = Snapshot(data: .init(title: "task"), path: .tasks)
+        task.create { result in
+            switch result {
+            case .success:
+                XCTFail()
+                 exp.fulfill()
+            case let .failure(error):
+                XCTAssertEqual(error as? SnapshotError, SnapshotError.emptyReference)
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 10.0)
+    }
 }
