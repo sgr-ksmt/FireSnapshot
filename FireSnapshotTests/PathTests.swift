22d553ecc57d9f20a969988102020fb8c3d2e443
sgr-ksmt <suguru-kishimoto@cookpad.com>
2019-10-10 16:09:50 +0900

    improved Paths tests.

--- FireSnapshotTests/PathTests.swift
+++ FireSnapshotTests/PathTests.swift
@@ -60,4 +60,45 @@ class PathTests: XCTestCase {
         XCTAssertEqual(CollectionPath<Mock>("mocks"), CollectionPath<Mock>("mocks"))
         XCTAssertNotEqual(CollectionPath<Mock>("mocks1"), CollectionPath<Mock>("mocks2"))
     }
+
+    func testAnyDocumentPath() {
+        XCTAssertTrue(AnyDocumentPath("mocks/xxx").isValid)
+        XCTAssertTrue(AnyDocumentPath("mocks/xxx/sub/yyy").isValid)
+        XCTAssertTrue(AnyCollectionPath("mocks").anyDocument("xxx").isValid)
+        XCTAssertTrue(AnyCollectionPath("mocks").anyDocument("xxx").anyCollection("sub").anyDocument("yyy").isValid)
+
+        XCTAssertFalse(AnyDocumentPath("").isValid)
+        XCTAssertFalse(AnyDocumentPath("/").isValid)
+        XCTAssertFalse(AnyDocumentPath("///").isValid)
+        XCTAssertFalse(AnyDocumentPath("/xxx").isValid)
+        XCTAssertFalse(AnyDocumentPath("//xxx").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks/").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks//").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks/xxx/sub").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks/xxx//yyy").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks//sub").isValid)
+        XCTAssertFalse(AnyDocumentPath("mocks//sub/yyy").isValid)
+
+        XCTAssertEqual(AnyDocumentPath("mocks/xxx"), AnyDocumentPath("mocks/xxx"))
+        XCTAssertNotEqual(AnyDocumentPath("mocks/xxx"), AnyDocumentPath("mocks/yyy"))
+    }
+
+    func testAnyCollectionPath() {
+        XCTAssertTrue(AnyCollectionPath("mocks").isValid)
+        XCTAssertTrue(AnyCollectionPath("mocks/xxx/sub").isValid)
+        XCTAssertTrue(AnyCollectionPath("mocks").anyDocument("xxx").anyCollection("sub").isValid)
+
+        XCTAssertFalse(AnyCollectionPath("").isValid)
+        XCTAssertFalse(AnyCollectionPath("/").isValid)
+        XCTAssertFalse(AnyCollectionPath("///").isValid)
+        XCTAssertFalse(AnyCollectionPath("mocks/xxx").isValid)
+        XCTAssertFalse(AnyCollectionPath("/xxx").isValid)
+        XCTAssertFalse(AnyCollectionPath("mocks/xxx/sub/yyy").isValid)
+        XCTAssertFalse(AnyCollectionPath("mocks/xxx//yyy").isValid)
+        XCTAssertFalse(AnyCollectionPath("mocks//yyy").isValid)
+
+        XCTAssertEqual(AnyCollectionPath("mocks"), AnyCollectionPath("mocks"))
+        XCTAssertNotEqual(AnyCollectionPath("mocks1"), AnyCollectionPath("mocks2"))
+    }
 }
