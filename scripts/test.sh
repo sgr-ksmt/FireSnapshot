da3ff09059b2e201324508c7e611d2210511e608
sgr-ksmt <melodydance.k.s@gmail.com>
2020-09-23 00:34:14 +0900

    fxi firebase-tools version.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@ cd `dirname $0`
 ./setup_emulator.sh
 
 cd ../firebase
-firebase emulators:exec "`pwd`/../scripts/unittest.sh" --only firestore
+firebase emulators:exec "`pwd`/../scripts/unittest.sh" --only firestore --debug
